4fabf30eb3443091f229f2ddf0c11f329ce55fdf
Nishant <vnishant101297@gmail.com>
2018-12-21 20:18:13 +0530

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -17,10 +17,17 @@ app.post('/todos',(req,res)=>{
     res.status(400).send(e);
   });
 });
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  },(e)=>{
+    res.status(400).send(e);
+  });
+});
 app.listen(3000,()=>{
   console.log('Started on port 3000');
 });
-
+module.exports={app};
 // var newUser=new User({
 //   email:'vnishant@gmail.com'
 // });
